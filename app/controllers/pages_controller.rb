87394d21fd4f68608466bf4f680b20fe976e89f2
Siri Myhre <siri.o.myhre@gmail.com>
2018-08-15 18:12:50 +0100

    fixed so that the navbar doesnt show when the user is not signed in, and that the user is redirected to the /forecasts when signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    unless current_user.nil?
+      redirect_to forecasts_path
+    end
   end
 end
