c1d73df4f4fccf7d2a9f36a712aead10282850d1
Siri Myhre <siri.o.myhre@gmail.com>
2018-08-16 17:50:38 +0100

    It is now possible to add and remove a session from the forecast dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ Rails.application.routes.draw do
     collection do
       get 'settings', to: "users#settings"
     end
-    resources :sessions, only: [:create, :destroy, :index]
+    #resources :sessions, only: [:create, :destroy, :index]
   end
   resources :spots, only: [:index]
   resources :favorite_spots, only: [:create, :destroy]
   resources :forecasts, only: [:index]
   #resources :sessions, only: [:create, :destroy, :index]
+  get 'mysessions', to: 'sessions#index', as: :mysessions
+  post 'mysessions/:id', to: 'sessions#create', as: :mysessions_create
+  delete 'mysessions/:id', to: 'sessions#destroy', as: :mysessions_destroy
 end
