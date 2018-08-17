bf75906e5b422ee2d3a1e830fd1ea26ad58f5d51
Emanuel Peroni <emanuelperoni@gmail.com>
2018-08-17 17:00:07 +0100

    start toggle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import "../components/bootstrap_slider";
 import "../components/slider";
 
 import {tab_listeners} from '../components/tabs';
+import {actionSwitch} from '../components/switch';
 import {actionSlider} from '../components/slider';
 tab_listeners();
+actionSwitch();
 actionSlider();
