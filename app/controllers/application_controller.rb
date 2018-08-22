dd33365fae0151381d55752494120c05c7bb5684
clement lattelais <clement.wt@gmail.com>
2018-08-22 11:41:01 +0100

    title stuff done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
   # Uncomment when you *really understand* Pundit!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def default_url_options
+  { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
