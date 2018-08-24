e486875203165c93f3d4fd35416e39986015be1c
Siri Myhre <siri.o.myhre@gmail.com>
2018-08-24 09:57:44 +0100

    changed welcome-page and ordered the sessions by date and timeslot

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
 
   def index
-    policy_scope(Session).includes(:forecast).order('forecasts.date asc')
-    @sessions = Session.where(user: current_user).joins(:forecast).where('forecasts.date >= ?', Date.today.to_s)
+    policy_scope(Session)
+    @sessions = Session.where(user: current_user).joins(:forecast).where('forecasts.date >= ?', Date.today.to_s).includes(:forecast).order('forecasts.date asc, forecasts.time_slot asc')
   end
 
   def create
